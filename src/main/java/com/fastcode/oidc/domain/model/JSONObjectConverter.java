5366db62295bd2e6735ab8989cdeae221f3f90c2
Muhammad Naeem <77039175+mnaeem99@users.noreply.github.com>
2025-09-30 20:10:58 +0500

    added jar based changes

--- src/main/java/com/fastcode/oidc/domain/model/JSONObjectConverter.java
+++ src/main/java/com/fastcode/oidc/domain/model/JSONObjectConverter.java
@@ -1,46 +1,46 @@
-package com.fastcode.oidc.domain.model;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
-
-@Converter
-public class JSONObjectConverter implements AttributeConverter<JSONObject, String> {
-    @Override
-    public String convertToDatabaseColumn(JSONObject jsonData) {
-        String json = "";
-        try {
-            if(jsonData != null) {
-                json = jsonData.toString();
-            }
-        }
-        catch (NullPointerException ex)
-        {
-            //extend error handling here if you want
-           // json = "";
-        }
-        return json;
-    }
-
-    @Override
-    public JSONObject convertToEntityAttribute(String jsonDataAsJson) {
-    	JSONParser parser = new JSONParser();
-		
-		Object obj =null;
-		try {
-		    if(jsonDataAsJson != null) {
-                obj = parser.parse(jsonDataAsJson);
-            }
-			JSONObject jsonData = (JSONObject) obj;
-			return jsonData;
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-		//	e.printStackTrace();
-			return null;
-		}
-
-    }
-}
+//package com.fastcode.oidc.domain.model;
+//
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.ParseException;
+//
+//import jakarta.persistence.AttributeConverter;
+//import jakarta.persistence.Converter;
+//
+//@Converter
+//public class JSONObjectConverter implements AttributeConverter<JSONObject, String> {
+//    @Override
+//    public String convertToDatabaseColumn(JSONObject jsonData) {
+//        String json = "";
+//        try {
+//            if(jsonData != null) {
+//                json = jsonData.toString();
+//            }
+//        }
+//        catch (NullPointerException ex)
+//        {
+//            //extend error handling here if you want
+//           // json = "";
+//        }
+//        return json;
+//    }
+//
+//    @Override
+//    public JSONObject convertToEntityAttribute(String jsonDataAsJson) {
+//    	JSONParser parser = new JSONParser();
+//
+//		Object obj =null;
+//		try {
+//		    if(jsonDataAsJson != null) {
+//                obj = parser.parse(jsonDataAsJson);
+//            }
+//			JSONObject jsonData = (JSONObject) obj;
+//			return jsonData;
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//		//	e.printStackTrace();
+//			return null;
+//		}
+//
+//    }
+//}
